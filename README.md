e5ae7f797cc5883f8742b2e0a0221c2f4db8e166
tiffaniechia <tiffaniekyrachia@gmail.com>
2015-01-01 19:17:03 +0000

    app now gets and posts data to url + changed to in-browser jsx compiler + edited file structure

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 ### React.js Beginners Tutorial
 This tutorial follows the one given by Facebook.
 
-*to run with webserver:  "$ python -m SimpleHTTPServer" 
+*to run with web server in order to get and post files: "$ node server.js"
--- public/src/comment_box.js
+++ public/src/comment_box.js
@@ -11,6 +11,21 @@ var CommentBox = React.createClass({
       }.bind(this)
     });
   },
+  handleCommentSubmit: function(comment){
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      type: 'POST',
+      data: comment,
+      success: function(data){
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err){
+        console.error(this.props.url, status, err.toString());
+
+      }.bind(this)
+    });
+  },
   getInitialState: function(){
     return{data: []};
   },
@@ -23,7 +38,7 @@ var CommentBox = React.createClass({
       <div className="commentBox"> 
         <h1>Comments</h1>
         <CommentList data={this.state.data} />
-        <CommentForm />
+        <CommentForm onCommentSubmit={this.handleCommentSubmit}/>
       </div>
     );
   }
@@ -58,66 +73,30 @@ var Comment = React.createClass({
 });
 
 var CommentForm = React.createClass({
+  handleSubmit: function(e){
+    e.preventDefault();
+    var author = this.refs.author.getDOMNode().value.trim();
+    var text = this.refs.text.getDOMNode().value.trim();
+    if(!text || !author){
+      return;
+    }
+    this.props.onCommentSubmit({author: author, text: text});
+    this.refs.author.getDOMNode().value = '';
+    this.refs.text.getDOMNode().value = '';
+    return;
+  },
   render: function(){
     return(
-      <div className="commentForm"> This is a commentForm div </div>
+      <form className="commentForm" onSubmit={this.handleSubmit}> 
+        <input type="text" placeholder="your name" ref="author" />
+        <input type="text" placeholder="comments" ref="text"/>
+        <input type="submit" value="post" />
+      </form>
     );
   }
 });
 
 React.render(
-  <CommentBox url="src/comments.json" pollInterval={500}/>, 
+  <CommentBox url="comments.json" pollInterval={500}/>, 
   document.getElementById('content')
  );
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
